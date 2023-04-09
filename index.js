202a27bfb50993b9ec011ea2c3e5eb105382d583
Minhaj ahmed <43043473+Minhaj10@users.noreply.github.com>
2023-04-09 17:05:18 +1000

    dynamic api,api perameter,access params

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const users=[
+    {id: 0, name:"minhaj"},
+    {id:1, name:"fahad"},
+]
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/users',(req,res)=>{
+    res.send('Here is my user')
+})
+
+app.get('/users/:id',(req,res)=>{
+    const id=req.params.id;
+    const user=users[id];
+    res.send(user);
 })
 
 app.listen(port, () => {
